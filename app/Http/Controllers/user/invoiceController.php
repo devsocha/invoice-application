4ea13df1d8d5f819d4832c541f9c981cccd6565f
Konrad Socha <konrad.socha5@gmail.com>
2023-02-21 22:55:44 +0100

    Maly update migracji bazy w tabeli invoices

--- app/Http/Controllers/user/invoiceController.php
+++ app/Http/Controllers/user/invoiceController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\invoice;
 use Illuminate\Http\Request;
 
 class invoiceController extends Controller
 {
     public function index(){
-        return view('user.invoice');
+        $invoices = invoice::all();
+        return view('user.invoice')->with([
+            'invoices'=>$invoices,
+        ]);
     }
 }
--- resources/views/user/invoice.blade.php
+++ resources/views/user/invoice.blade.php
@@ -1,5 +1,31 @@
 @extends('user.layout.layout')
-@section('title','Ustawienia konta')
+@section('title','Faktury')
 @section('content')
-    dziala faktury
+    <div class="row g-3 m-1">
+        <a href="#" class="col-auto btn btn-primary m-3">Wystaw fakturę</a>
+    </div>
+
+    <table class="table text-center">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Numer faktury</th>
+            <th scope="col">Kwota netto</th>
+            <th scope="col">Status</th>
+            <th scope="col">Akcje</th>
+        </tr>
+        </thead>
+        <tbody>
+{{--        @foreach($invoices as $invoice)--}}
+{{--            <tr>--}}
+{{--                <th scope="row">{{$loop->iteration}}</th>--}}
+{{--                <td>{{$invoice->nazwa}}</td>--}}
+{{--                <td>{{$invoice->kwota netto}}</td>--}}
+{{--                <td>--}}
+{{--                    <a href="{{route('companyEdit',['id'=>$company->id])}}" class="btn btn-success">Edytuj</a>--}}
+{{--                    <a href="{{route('companyDelete',['id'=>$company->id])}}" class="btn btn-danger">Usuń</a>--}}
+{{--                </td>--}}
+{{--            </tr>--}}
+{{--        @endforeach--}}
+    </table>
 @endsection
