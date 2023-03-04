1775fe63ab7b5b51f40fdb9b9422434e0704773e
Konrad Socha <konrad.socha5@gmail.com>
2023-03-04 17:32:14 +0100

    Dodanie wszystkich elementów dashboarda oraz dodanie middleware admin

--- app/Http/Controllers/user/homeController.php
+++ app/Http/Controllers/user/homeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use App\Models\company;
+use App\Models\invoice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,8 +12,18 @@ class homeController extends Controller
 {
     public function index(){
         $companies = company::take(5)->orderBy('created_at','desc')->get();
+        $data = date('d.m.Y',strtotime('-7day'));
+        $dataMonth = date('d.m.Y',strtotime('-30day'));
+        $invoiceLastMonth = invoice::where('created_at','>',$dataMonth)->count();
+        $invoicePaidLastMonth = invoice::where('created_at','>',$dataMonth)->where('status','Zapłacono')->count();
+        $invoiceNoPaidLastMonth = invoice::where('created_at','>',$dataMonth)->where('status','Wystawiona')->count();
+        $invoices = invoice::where('created_at','>',$data)->take(5)->get();
         return view('user.home')->with([
             'companies'=>$companies,
+            'invoices'=>$invoices,
+            'invoiceLastMonth'=>$invoiceLastMonth,
+            'invoicePaidLastMonth'=>$invoicePaidLastMonth,
+            'invoiceNoPaidLastMonth'=>$invoiceNoPaidLastMonth,
         ]);
     }
 }
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -20,12 +20,57 @@
                     <td>{{$company->miasto}}</td>
                 </tr>
             @endforeach
-
             </tbody>
         </table>
     </div>
-    <div class="col-6">sss</div>
-    <div class="col-3">sss</div>
+    <div class="col-6"style="border:1px solid black; height: 270px;border-radius: 5%">
+        <center> Ostatnio wystawione faktury</center>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nazwa</th>
+                <th scope="col">Wartość netto</th>
+                <th scope="col">Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($invoices as $invoice)
+                <tr>
+                    <th scope="row">{{$loop->iteration}}</th>
+                    <td>{{$invoice->nazwa}}</td>
+                    <td>{{$invoice->kwotanetto*$invoice->ile}}</td>
+                    <td>{{$invoice->status}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="col-3"style="border:1px solid black; height: 270px;border-radius: 5%">
+        <center> Podsumowanie (ostatnie 30 dni)</center>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Nazwa</th>
+                <th scope="col">Ilość</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Wystawione faktury: </td>
+                    <td>{{$invoiceLastMonth}}</td>
+                </tr>
+                <tr>
+                    <td>Opłacone faktury: </td>
+                    <td>{{$invoicePaidLastMonth}}</td>
+                </tr>
+                <tr>
+                    <td>Faktury oczekujące na płatność: </td>
+                    <td>{{$invoiceNoPaidLastMonth}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
